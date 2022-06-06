c748828e306e861aa5d17da115ad7d181755c365
Neoremos <105473267+Neoremos@users.noreply.github.com>
2022-06-06 09:25:22 +0800

    Update digiCalc.js

--- digiCalc/digiCalc.js
+++ digiCalc/digiCalc.js
@@ -14,11 +14,7 @@ clicks.map(click => {
                 display.innerText = display.innerText.slice(0, -1);
                 break;
             case '=':
-                try {
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = 'Error!';
-                }
+                display.innerText = parseFloat(display.innerText);
                 break;
             default :
                 display.innerText += e.target.innerText;
