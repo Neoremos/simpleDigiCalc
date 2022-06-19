099a899da39e1528b4ad3acef9d16f53473a0bac
Neoremos <105473267+Neoremos@users.noreply.github.com>
2022-06-19 23:41:21 +0800

    Update digiCalc.js

--- digiCalc/digiCalc.js
+++ digiCalc/digiCalc.js
@@ -13,7 +13,7 @@ clicks.map(click => {
                 break;
             case '=':
                 try {
-                    display.innerText = eval(display.innerText.slice(0, -1));
+                    display.innerText = eval(display.innerText);
                 } catch {
                     display.innerText = 'Error!';
                 }
